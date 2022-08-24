3c5a0efdce182732a3987286925015f5c3c88be2
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-24 00:36:41 +0000

    inflated the root

--- app/src/main/java/android/ui/gradisonfood/MainActivity.java
+++ app/src/main/java/android/ui/gradisonfood/MainActivity.java
@@ -3,12 +3,17 @@ package android.ui.gradisonfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.ui.gradisonfood.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    // viewBinding
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
