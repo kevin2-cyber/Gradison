5a3e8e86fe568acf7403f003aa389d33b0debc3b
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-07 04:51:08 +0000

    new

--- app/src/main/java/android/ui/gradisonfood/ui/MenuActivity.java
+++ app/src/main/java/android/ui/gradisonfood/ui/MenuActivity.java
@@ -3,12 +3,16 @@ package android.ui.gradisonfood.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.ui.gradisonfood.databinding.ActivityMenuBinding;
 
 public class MenuActivity extends AppCompatActivity {
 
+    ActivityMenuBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(binding.getRoot());
     }
 }
