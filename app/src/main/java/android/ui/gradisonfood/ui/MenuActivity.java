3a279ac17b950a61e11a3f36727744751c953017
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-07 06:12:05 +0000

    bugs have started coming back again😒

--- app/src/main/java/android/ui/gradisonfood/ui/MenuActivity.java
+++ app/src/main/java/android/ui/gradisonfood/ui/MenuActivity.java
@@ -2,6 +2,7 @@ package android.ui.gradisonfood.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.ui.gradisonfood.databinding.ActivityMenuBinding;
 
@@ -14,5 +15,16 @@ public class MenuActivity extends AppCompatActivity {
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+
+
+        // Font path
+        String fontPath = "fonts/montserrat_regular.ttf";
+
+        // Loading Font Face
+        Typeface m_typeFace = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Applying font
+        binding.menu.setTypeface(m_typeFace);
     }
 }
