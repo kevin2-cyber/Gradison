8e49b9707d98602dd7c3db3fa8c2af659dbdbd97
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-13 23:42:06 +0000

    new

--- app/src/main/java/android/ui/gradisonfood/model/FoodModel.java
+++ app/src/main/java/android/ui/gradisonfood/model/FoodModel.java
@@ -3,11 +3,13 @@ package android.ui.gradisonfood.model;
 public class FoodModel {
 
     private int image;
+    private int backgroundColor;
     private String foodName;
 
-    public FoodModel(int image, String foodName) {
+    public FoodModel(int image, String foodName, int backgroundColor) {
         this.image = image;
         this.foodName = foodName;
+        this.backgroundColor = backgroundColor;
     }
 
     public int getImage() {
@@ -16,6 +18,14 @@ public class FoodModel {
 
     public void setImage(int image) {
         this.image = image;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
     }
 
     public String getFoodName() {
