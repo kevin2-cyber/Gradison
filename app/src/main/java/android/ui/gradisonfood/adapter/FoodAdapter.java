5d9b001056c4cd4ee865a48c2638a647c176704b
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-13 23:53:49 +0000

    removed the nullable annotation

--- app/src/main/java/android/ui/gradisonfood/adapter/FoodAdapter.java
+++ app/src/main/java/android/ui/gradisonfood/adapter/FoodAdapter.java
@@ -4,7 +4,6 @@ import android.ui.gradisonfood.model.FoodModel;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -13,7 +12,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
 
     private ArrayList<FoodModel> foodList;
 
-    @Nullable
+    @NonNull
     @Override
     public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
