dd78de0964781eeb73a42e7ef7cd9371b2376cbc
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-13 22:39:08 +0000

    food

---
+++ app/src/main/java/android/ui/gradisonfood/adapter/FoodAdapter.java
@@ -0,0 +1,24 @@
+package android.ui.gradisonfood.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/android/ui/gradisonfood/adapter/FoodViewHolder.java
@@ -0,0 +1,12 @@
+package android.ui.gradisonfood.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FoodViewHolder extends RecyclerView.ViewHolder {
+    public FoodViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
