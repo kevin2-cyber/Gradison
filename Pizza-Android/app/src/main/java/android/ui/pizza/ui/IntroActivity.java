bda530bf2aaae0c9d20f2c716185e66c6c18a210
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-21 03:03:14 +0000

    trying this out hopes it works

--- Pizza-Android/app/src/main/java/android/ui/pizza/ui/IntroActivity.java
+++ Pizza-Android/app/src/main/java/android/ui/pizza/ui/IntroActivity.java
@@ -2,7 +2,9 @@ package android.ui.pizza.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.ui.pizza.MainActivity;
 import android.ui.pizza.databinding.ActivityIntroBinding;
 
 public class IntroActivity extends AppCompatActivity {
@@ -14,5 +16,15 @@ public class IntroActivity extends AppCompatActivity {
         binding = ActivityIntroBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+        binding.btnStart.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
