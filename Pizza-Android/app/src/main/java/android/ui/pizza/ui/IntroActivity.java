781c70ef12b40b32d71ca2ffcc9a44ed0fd5b676
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-21 01:14:05 +0000

    added intro activity

---
+++ Pizza-Android/app/src/main/java/android/ui/pizza/ui/IntroActivity.java
@@ -0,0 +1,15 @@
+package android.ui.pizza.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
