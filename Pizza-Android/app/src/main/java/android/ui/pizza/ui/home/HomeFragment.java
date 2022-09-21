736be75b64935d32db223659c08bbf75a12c3330
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-21 04:49:09 +0000

    cleared the view model instance

--- Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/HomeFragment.java
+++ Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/HomeFragment.java
@@ -24,8 +24,8 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/HomeViewModel.java
+++ Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+//        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
