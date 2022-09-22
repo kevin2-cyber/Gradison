813afa083a5c3cab5d207374ef0a26483063d598
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-22 14:09:33 +0000

    done creating RatingCard  Model

--- Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/model/RatingModel.java
+++ Pizza-Android/app/src/main/java/android/ui/pizza/ui/home/model/RatingModel.java
@@ -1,3 +1,58 @@
 package android.ui.pizza.ui.home.model;
 
-public class RatingModel {}
+public class RatingModel {
+
+    private String rating;
+    private int burgerImage;
+    private String burgerName;
+    private String burgerContent;
+    private String price;
+
+    public RatingModel(String rating, int burgerImage, String burgerName, String burgerContent, String price) {
+        this.rating = rating;
+        this.burgerImage = burgerImage;
+        this.burgerName = burgerName;
+        this.burgerContent = burgerContent;
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getBurgerImage() {
+        return burgerImage;
+    }
+
+    public void setBurgerImage(int burgerImage) {
+        this.burgerImage = burgerImage;
+    }
+
+    public String getBurgerName() {
+        return burgerName;
+    }
+
+    public void setBurgerName(String burgerName) {
+        this.burgerName = burgerName;
+    }
+
+    public String getBurgerContent() {
+        return burgerContent;
+    }
+
+    public void setBurgerContent(String burgerContent) {
+        this.burgerContent = burgerContent;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
