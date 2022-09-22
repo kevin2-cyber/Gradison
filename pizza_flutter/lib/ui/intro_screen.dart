6883c83c27a7c49fead7296c81929ef03d811565
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-22 23:32:45 +0000

    new

---
+++ pizza_flutter/lib/ui/intro_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
