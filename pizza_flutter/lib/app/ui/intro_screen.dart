d020c3dd0d338f9450ab0e5faecc16aecde7dba7
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-03 10:11:10 +0000

    using utils as a testing folder

--- pizza_flutter/lib/app/ui/intro_screen.dart
+++ pizza_flutter/lib/app/ui/intro_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pizza_flutter/app/ui/demo_page_view.dart';
+import '../../utils/demo_page_view.dart';
 import '/core/app_colors.dart';
 
 class IntroScreen extends StatefulWidget {
