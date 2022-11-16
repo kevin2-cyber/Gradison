8bcd2700c70df200ce8e2986043b9b35cbdfc15f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-16 20:56:02 +0000

    new

--- pizza_flutter/lib/core/constants.dart
+++ pizza_flutter/lib/core/constants.dart
@@ -1,11 +1,2 @@
 
 
-import 'package:flutter/material.dart';
-
-class AppConstants {
-  Size sizeConfig(BuildContext  context) {
-     final kSpacingX = MediaQuery.of(context).size;
-     return kSpacingX;
-  }
-
-}
