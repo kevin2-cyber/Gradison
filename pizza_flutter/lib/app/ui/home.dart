1526a82b9266cecadc623b1a1583621cae06022f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-17 02:35:33 +0000

    done with categories

--- pizza_flutter/lib/app/ui/home.dart
+++ pizza_flutter/lib/app/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/categories.card.dart';
 import '/core/core.dart';
 
 class Home extends StatefulWidget {
@@ -9,12 +10,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   // Form key = GlobalKey(debugLabel: "key") as Form;
 
   @override
   Widget build(BuildContext context) {
-    var kSpacingX  = MediaQuery.of(context).size;
+    var kSpacingX = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -24,18 +24,18 @@ class _HomeState extends State<Home> {
               Row(
                 children: const <Widget>[
                   Icon(
-                      Icons.location_on,
+                    Icons.location_on,
                     color: AppColors.kPrimary,
                   ),
                   Text(
-                      'Nevada, US',
+                    'Nevada, US',
                     style: TextStyle(
                       fontSize: 15,
                     ),
                   ),
                   Icon(
-                      Icons.keyboard_arrow_down_outlined,
-                  color: AppColors.kPrimary,
+                    Icons.keyboard_arrow_down_outlined,
+                    color: AppColors.kPrimary,
                   ),
                 ],
               ),
@@ -45,7 +45,7 @@ class _HomeState extends State<Home> {
               Row(
                 children: <Widget>[
                   const Text(
-                      'Order Your Food\nFast and Free',
+                    'Order Your Food\nFast and Free',
                     style: TextStyle(
                       fontSize: 25,
                     ),
@@ -72,10 +72,9 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         focusedBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.kPrimary,
-                          )
-                        ),
+                            borderSide: BorderSide(
+                          color: AppColors.kPrimary,
+                        )),
                       ),
                     ),
                   ),
@@ -83,10 +82,8 @@ class _HomeState extends State<Home> {
                     height: kSpacingX.height * 0.1,
                     width: kSpacingX.width * 0.3,
                     child: IconButton(
-                      onPressed: (){},
-                      icon: Image.asset(
-                          AppAssets.kFrameImage
-                      ),
+                      onPressed: () {},
+                      icon: Image.asset(AppAssets.kFrameImage),
                     ),
                   ),
                 ],
@@ -95,13 +92,47 @@ class _HomeState extends State<Home> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: const [
                   Text(
-                      'Categories',
+                    'Categories',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: kSpacingX.height * 0.01,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: const [
+                    CategoryCard(
+                      bgColor: AppColors.kPrimary,
+                      asset: '🍔',
+                      title: 'Burger',
+                      textColor: Colors.white,
+                    ),
+                    CategoryCard(
+                      bgColor: Colors.white,
+                      asset: '🍕',
+                      title: 'Pizza',
+                      textColor: Colors.black,
+                    ),
+                    CategoryCard(
+                      bgColor: Colors.white,
+                      asset: '🌭',
+                      title: 'Sandwich',
+                      textColor: Colors.black,
+                    ),
+                    CategoryCard(
+                      bgColor: Colors.white,
+                      asset: '🍔',
+                      title: 'Burger',
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
--- pizza_flutter/lib/app/widgets/categories.card.dart
+++ pizza_flutter/lib/app/widgets/categories.card.dart
@@ -5,25 +5,41 @@ class CategoryCard extends StatelessWidget {
   final Color bgColor;
   final String asset;
   final String title;
-  const CategoryCard({Key? key, required this.bgColor, required this.asset, required this.title}) : super(key: key);
+  final Color textColor;
+  const CategoryCard({Key? key, required this.bgColor, required this.asset, required this.title, required this.textColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: bgColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: OutlinedButton(
-        onPressed: () {},
-        style: OutlinedButton.styleFrom(
-          side: const BorderSide(width: 5.0, color: AppColors.kPrimary),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.35,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: Card(
+        color: bgColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
-        child: Row(
-          children: [
-            Image.asset(asset),
-            Text(title),
-          ],
+        child: OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: AppColors.kPrimary),
+          ),
+          child: Row(
+            children: [
+              Text(
+                  asset,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                  title,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: textColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
