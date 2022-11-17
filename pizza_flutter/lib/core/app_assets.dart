e613652fe30f168769b534f72f31c0cb6fac291d
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-17 03:02:44 +0000

    updated the names of the assets

--- pizza_flutter/lib/core/app_assets.dart
+++ pizza_flutter/lib/core/app_assets.dart
@@ -3,12 +3,12 @@ class AppAssets {
   static String kFrameImage = 'assets/images/frame.png';
   static String kDeliveryImage = 'assets/images/delivery.png';
   static String kBackImage = "assets/images/back.jpg";
-  static String kCircleImage  = "assets/images/circle.png";
+  static String kCircleImage  = "assets/icon/circle.png";
   static String kImageBkg = "assets/images/imagebkg.jpg";
   static String kImg = "assets/images/img.png";
-  static String kImg1 = "assets/images/home.png";
-  static String kShopping = "assets/images/shopping.png";
-  static String kStar = "assets/images/star.png";
+  static String kImg1 = "assets/icons/home.png";
+  static String kShopping = "assets/icons/shopping.png";
+  static String kStar = "assets/icons/star.png";
   static String kCheeseBurger = "assets/images/cheseburger.png";
   static String kChickenBurger = "assets/images/chicken.png";
 }
