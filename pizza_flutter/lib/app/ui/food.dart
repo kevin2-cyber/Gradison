94857c1436f9b71f804e5f9f10e36d392d88f52e
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-18 11:26:07 +0000

    done with design

--- pizza_flutter/lib/app/ui/food.dart
+++ pizza_flutter/lib/app/ui/food.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_flutter/core/core.dart';
 
-class Food extends StatelessWidget {
+enum MenuItem {
+  large,
+  medium,
+  small
+}
+
+class Food extends StatefulWidget {
   const Food({Key? key}) : super(key: key);
 
   @override
+  State<Food> createState() => _FoodState();
+}
+
+class _FoodState extends State<Food> {
+  @override
   Widget build(BuildContext context) {
+    String newVal = '';
     return Scaffold(
       backgroundColor: AppColors.kFoodbg,
       body: Container(
@@ -117,6 +129,198 @@ class Food extends StatelessWidget {
                           ),
                         ),
                       ],
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.05).height,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: AppConstants.sizeConfig(context, 0.1).height,
+                          width: AppConstants.sizeConfig(context, 0.28).width,
+                          child: OutlinedButton(
+                            onPressed: (){},
+                            style: OutlinedButton.styleFrom(
+                              side: const BorderSide(
+                                color: AppColors.kPrimary,
+                              )
+                            ),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'Size',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: AppColors.kPrimary,
+                                      ),
+                                    ),
+                                    PopupMenuButton<MenuItem>(
+                                      iconSize: 16,
+                                      icon: const Icon(
+                                          Icons.keyboard_arrow_down,
+                                        color: AppColors.kPrimary,
+                                      ),
+                                      onSelected: (value) {
+                                        if(value == MenuItem.large) {
+                                          setState(() {
+                                            newVal = 'Large';
+                                          });
+                                        } else if(value == MenuItem.medium) {
+                                          setState(() {
+                                            newVal = 'Medium';
+                                          });
+                                        } else {
+                                          setState(() {
+                                            newVal = 'Small';
+                                          });
+                                        }
+                                      },
+                                      itemBuilder: (context) => const [
+                                        PopupMenuItem(
+                                          value: MenuItem.large,
+                                          child: Text('Large'),
+                                        ),
+                                        PopupMenuItem(
+                                          value: MenuItem.medium,
+                                          child: Text('Medium'),
+                                        ),
+                                        PopupMenuItem(
+                                          value: MenuItem.small,
+                                          child: Text('Small'),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const Text(
+                                    'Medium',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: AppColors.kPrimary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: AppConstants.sizeConfig(context, 0.1).height,
+                          width: AppConstants.sizeConfig(context, 0.28).width,
+                          child: OutlinedButton(
+                            onPressed: (){},
+                            style: OutlinedButton.styleFrom(
+                              side: const BorderSide(
+                                color: AppColors.kPrimary,
+                              )
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: const [
+                                Text(
+                                  'Energy',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: AppColors.kPrimary,
+                                  ),
+                                ),
+                                Text(
+                                    '554 KCal',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: AppConstants.sizeConfig(context, 0.1).height,
+                          width: AppConstants.sizeConfig(context, 0.28).width,
+                          child: OutlinedButton(
+                            onPressed: (){},
+                            style: OutlinedButton.styleFrom(
+                              side: const BorderSide(
+                                color: AppColors.kPrimary,
+                              )
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: const [
+                                Text(
+                                  'Delivery',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: AppColors.kPrimary,
+                                  ),
+                                ),
+                                Text(
+                                    '45 min',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.02).height,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text(
+                            'About',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.05).height,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text(
+                            'Crispy seasoned chicken breast, topped with'
+                            '\nmandatory melted cheese and piled onto soft rolls'
+                            '\nwith onion, avocado, lettuce, tomato and garlic mayo\n'
+                            '(if ordered). ',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.05).height,
+                    ),
+                    MaterialButton(
+                      minWidth: AppConstants.sizeConfig(context, 0.9).width,
+                      height: AppConstants.sizeConfig(context, 0.05).height,
+                      onPressed: (){},
+                      color: AppColors.kPrimary,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: const Text(
+                          'Add to Cart',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white
+                        ),
+                      ),
                     ),
                   ],
                 ),
